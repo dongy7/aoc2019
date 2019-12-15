367ac2954f1cc77c82e1afdfb4583940607175ca
Dong Yeop Lee <dongy7@gmail.com>
2019-12-14 20:00:10 -0600

    day 2 solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,52 @@
-pub fn test() {
-  println!("Hello world!");
+pub mod day2 {
+  fn str_to_vec(text: &str) -> Vec<u32> {
+    text.split(',').map(|s| s.parse().unwrap()).collect()
+  }
+
+  pub fn exec(mut mem: Vec<u32>) -> u32 {
+    let mut ip: usize = 0;
+    loop {
+      let opcode = mem[ip];
+      match opcode {
+        1 => {
+          let addr1 = mem[ip + 1] as usize;
+          let addr2 = mem[ip + 2] as usize;
+          let dest_addr = mem[ip + 3] as usize;
+          let sum = mem[addr1] + mem[addr2];
+          mem[dest_addr] = sum;
+          ip += 4;
+        }
+        2 => {
+          let addr1 = mem[ip + 1] as usize;
+          let addr2 = mem[ip + 2] as usize;
+          let dest_addr = mem[ip + 3] as usize;
+          let product = mem[addr1] * mem[addr2];
+          mem[dest_addr] = product;
+          ip += 4;
+        }
+        99 => break,
+        _ => {
+          println!("Unknown opcode: {}", opcode);
+          break;
+        }
+      }
+    }
+
+    mem[0]
+  }
+
+  pub fn run(text: &str) -> u32 {
+    let mem = str_to_vec(text);
+
+    exec(mem)
+  }
+
+  pub fn run_input(text: &str, noun: u32, verb: u32) -> u32 {
+    let mut mem = str_to_vec(text);
+    mem[1] = noun;
+    mem[2] = verb;
+    exec(mem)
+  }
 }
 
 pub mod day1 {
@@ -36,5 +83,13 @@ mod tests {
     assert_eq!(day1::compute_fuel2(14), 2);
     assert_eq!(day1::compute_fuel2(1969), 966);
     assert_eq!(day1::compute_fuel2(100756), 50346);
+  }
+
+  #[test]
+  fn test_process() {
+    assert_eq!(day2::run("1,9,10,3,2,3,11,0,99,30,40,50"), 3500);
+    assert_eq!(day2::run("1,0,0,0,99"), 2);
+    assert_eq!(day2::run("2,3,0,3,99"), 2);
+    assert_eq!(day2::run("1,1,1,4,99,5,6,0,99"), 30);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aoc::day1;
+use aoc::day2;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -22,6 +23,33 @@ fn main() {
 
     println!("Day 1 Part 2");
     println!("Fuel requirement: {}", sum2);
+
+    println!("Day 2");
+    run_day2();
+}
+
+fn run_day2() {
+    if let Ok(lines) = read_lines("data/day2.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("Value at position 0: {}", day2::run_input(&ip, 12, 2));
+
+                for noun in 0..100 {
+                    for verb in 0..100 {
+                        let res = day2::run_input(&ip, noun, verb);
+                        if res == 19690720 {
+                            println!(
+                                "Noun: {}, Verb: {}, answer: {}",
+                                noun,
+                                verb,
+                                100 * noun + verb
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
